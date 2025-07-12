9e04e113ecf17b638392a738856d5d7fdf6fbb2d
Adarsh Singh <adarsh22082012@gmail.com>
2025-07-13 00:55:15 +0530

    nodejs streams

---
+++ 14nodeStreams/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const fs = require('fs');
+const status = require('express-status-monitor');
+
+const app = express();
+const PORT = 3000;
+
+app.use(status());
+
+// app.get('/',(req, res) => {
+//   fs.readFile("./sample.txt",(err,data) => {
+//     res.end(data);
+//   })
+// });
+app.get('/',(req, res) => {
+  const stream = fs.createReadStream("./sample.txt","utf-8");
+  stream.on("data", (chunk) => res.write(chunk));
+  stream.on("end", () => res.end());
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
